5fe793b0068f467cd065c1119a2f3c257100499b
sifan <718662552@qq.com>
2021-04-12 19:55:20 +0800

    feat: 完善界面

--- pages/check/index.js
+++ pages/check/index.js
@@ -1,4 +1,7 @@
 // pages/check/index.js
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
+const util = require('../../utils/util.js')
+
 Page({
 
   /**
@@ -6,14 +9,16 @@ Page({
    */
   data: {
     searchVal: "",
-    list: [{}, {}]
+    siteData: [],
+    checkbox: {},
+    isComplete: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getList(true);
   },
 
   /**
@@ -66,22 +71,82 @@ Page({
   onShareAppMessage: function () {
 
   },
-  onChange(e) {
+  handleSearch(e) {
     this.setData({
       searchVal: e.detail,
     });
+  },
+  handleCheckbox(e) {
+    let { no } = e.target.dataset;
 
-    console.log(this.data.value)
+    if(!no) {
+      no = e.currentTarget.dataset.no || '';
+    }
+
+    if(no) {
+      const checkbox = {
+        ...this.data.checkbox,
+        [no]: !this.data.checkbox[no]
+      };
+
+      const isCheckbox = Object.keys(checkbox).some(key => {
+        return checkbox[key];
+      })
+
+      this.setData({
+        checkbox,
+        isCheckbox
+      })
+    }
+  },
+  handleTabs(e) {
+    const { index } = e.detail || {};
+
+    this.getList(index !== 1);
   },
   handleCopy(e) {
-    const { number } = e.target.dataset || {};
+    const { no } = e.target.dataset || {};
 
     wx.setClipboardData({
-      data: number,
+      data: no,
       success: function () {
         // 添加下面的代码可以复写复制成功默认提示文本`内容已复制` 
         wx.hideToast();
+
+        Toast('复制成功！');
       }
     })
+  },
+  getList(isSend) {
+    this.setData({
+      isComplete: false,
+      siteData: [],
+      checkbox: {}
+    })
+
+    setTimeout(() => {
+      const data = isSend ? [{
+        name: '站点之家',
+        addr: '广东省广州市天河区石牌村25号',
+        list: [{
+          no: 'SF1309548419093',
+          name: '顺丰快递',
+          info: {
+            addr: ['那然格市', '那然色布斯台音布拉格市'],
+            name: ['小天', '小凡']
+          },
+          code: '9650',
+          state: '今天到站',
+          time: util.formatTime(new Date()),
+          color: '#000'
+        }],
+        isCheckbox: false
+      }] : []
+
+      this.setData({
+        isComplete: true,
+        siteData: data
+      })
+    }, 1000)
   }
 })
