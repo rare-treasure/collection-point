6efd389d197063f14e54ae3a51eab4110ab5f9de
sifan <718662552@qq.com>
2021-04-25 19:00:03 +0800

    feat: 完善页面

--- pages/check/index.js
+++ pages/check/index.js
@@ -1,5 +1,7 @@
 // pages/check/index.js
 import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+
 const util = require('../../utils/util.js')
 
 Page({
@@ -11,8 +13,19 @@ Page({
     searchVal: "",
     siteData: [],
     checkbox: {},
-    isRefresher: true,
-    isComplete: false
+    colors: {
+      '顺丰快递': '#231F20',
+      '圆通速递': '#47086C',
+      '中通速递': '#1296db',
+      '百世汇通': '#00253A',
+      '申通快递': '#d81e06',
+      '韵达快递': '#F5C51D',
+    },
+    activeTabIdx: 0,
+    isRefresher: false,
+    isComplete: false,
+    isEndPage: false,
+    isLoading: false
   },
 
   /**
@@ -66,7 +79,12 @@ Page({
   handleTabs(e) {
     const { index } = e.detail || {};
 
-    this.getList(index !== 1);
+    this.setData({
+      isRefresher: true,
+      activeTabIdx: index,
+      siteData: [],
+      isComplete: false
+    })
   },
   handleCopy(e) {
     const { no } = e.target.dataset || {};
@@ -81,44 +99,69 @@ Page({
       }
     })
   },
-  handlePickUp() {
+  handlePickUp(e) {
+    const { all } = e.target.dataset;
+    const isAll = all === '1';
+
+    Dialog.confirm({
+      title: '取件提醒',
+      message: '是否要取出当前站点' + (!isAll ? '选中' : '全部') + '的快递？',
+      zIndex: 99999
+    }).then(() => {
+      // 调接口
+      // 接口成功后，手动匹配数据删除对应的订单
+
+    })
+  },
+  handleRejection() {
 
   },
-  getList(isSend, isAdd = false) {
+  getList(isAdd = false) {
     const siteData = this.data.siteData;
     this.setData({
-      checkbox: {},
-      ...(isAdd ? {
-        isRefresher: true
-      } : {})
+      checkbox: {}
     })
+    const isSend = this.data.activeTabIdx === 1;
+    const expressNameList = Object.keys(this.data.colors);
 
     const pr = new Promise((resolve, reject) => {
       setTimeout(() => {
-        const data = isSend ? new Array(Math.ceil(Math.random() * 10)).join(',').split(',').map((item, idx) => {
+        const data = !isSend ? new Array(3).join(',').split(',').map((item, idx) => {
           return {
             name: '站点之家' + (idx + 1) +'号', 
             addr: '广东省广州市天河区石牌村' + Math.floor(Math.random() * 100) * (idx + 1) +'号',
-            list: [{
-              no: 'SF' + Math.floor(Math.random() * Date.now()),
-              name: '顺丰快递',
-              info: {
-                addr: ['那然格市', '那然色布斯台音布拉格市'],
-                name: ['小天', '小凡']
-              },
-              code: Math.floor(Math.random() * 9000 + 1000),
-              state: '今天到站',
-              time: util.formatTime(new Date()),
-              color: '#000'
-            }],
+            list: new Array(10).join(',').split(',').map((sub, subIdx) => {
+              const expressName = expressNameList[Math.floor(Math.random() * 5)];
+
+              return {
+                no: (expressName === '顺丰快递' ? 'SF' : '') + Math.floor(Math.random() * Date.now() * (subIdx + 1)),
+                name: expressName,
+                info: {
+                  addr: ['那然格市', '那然色布斯台音布拉格市'],
+                  name: ['小天', '小凡']
+                },
+                code: Math.floor(Math.random() * 9000 + 1000),
+                state: '今天到站',
+                time: util.formatTime(new Date()),
+                color: this.data.colors[expressName] || ''
+              }
+            }),
             isCheckbox: false
           }
-        }) : []
+        }) : [];
+
+        const { isRefresher, isComplete } = this.data;
+        const isEndPage = Math.random() > 0.5;
 
         this.setData({
-          ...(isAdd? {} : { isComplete: true, 
-            isRefresher: false, }),
-          siteData: isAdd ? [...siteData, ...data] : data
+          ...(isRefresher ? {
+            isRefresher: false
+          } : {}),
+          ...(!isComplete ? {
+            isComplete: true
+          } : {}),
+          siteData: isAdd ? [...siteData, ...data] : data,
+          isEndPage
         })
 
         resolve();
@@ -128,15 +171,22 @@ Page({
     return pr;
   },
   handleRefreshList() {
-    this.getList(true)
+    this.setData({
+      isRefresher: true,
+      isLoading: false
+    })
+
+    this.getList().finally(() => {
+      this.setData({
+        isLoading: true
+      })
+    })
   },
   handleLoadMoreList() {
-    wx.showLoading({
-      title: '加载中…'
-    });
+    if(this.data.isEndPage) {
+      return;
+    }
 
-    this.getList(true, true).finally(() => {
-      wx.hideLoading()
-    });
+    this.getList(true);
   }
 })
